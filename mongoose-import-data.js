4353ff67cfa3740ffcd4099201769905f1ea9ecf
Kris Sullivan <55643073+ksully1993@users.noreply.github.com>
2020-10-14 00:47:43 -0400

    Update mongoose-import-data.js
    added the use of minimist to allow all arguments to easily be passed through the command line

--- mongoose-import-data.js
+++ mongoose-import-data.js
@@ -1,19 +1,23 @@
-// script to import json file data to db using mongoose models
-// command line argument format:
-// node import-data.js model command path/to/file
-// DB and password should be defined in config.env file
-// Example: DATABASE=mongodb://localhost:27017/yourdbname
-//          DATABASE_PASSWORD=yourdbpassword
+/* script to import json file data to db using mongoose models
+command line argument format:
+node import-data.js command model "name" path/to/file
+Example: node import-data.js import Vehicle "Glasco Heating & Air Conditionging" data/vehicle-data.json
+DB and password should be defined in config.env file
+Example: DATABASE=mongodb://localhost:27017/yourdbname
+         DATABASE_PASSWORD=yourdbpassword */
 
 const fs = require('fs');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const argv = require('minimist')(process.argv.slice(2));
 
+// Models already defined in separate files
 const Vehicle = require('../models/vehicleModel');
 const Customer = require('../models/customerModel');
 const Service = require('../models/serviceModel');
 const Operator = require('../models/operatorModel');
 
+// path to config.env file(from working directory, not relative path)
 dotenv.config({ path: './config.env' });
 
 // Connect to DB
@@ -35,14 +39,9 @@ mongoose
     process.exit();
   });
 
-const jsonFile = JSON.parse(fs.readFileSync(process.argv[4], 'utf-8'));
-if (!jsonFile) {
-  return console.log('File not found. Verify path and try again.');
-}
-
 // Select correct model for query
-let model;
-switch (process.argv[2]) {
+let model = null;
+switch (argv._[1]) {
   case 'Vehicle':
     model = Vehicle;
     break;
@@ -64,7 +63,7 @@ switch (process.argv[2]) {
 const importData = async (query) => {
   try {
     await query;
-    console.log(`Successfully imported data to ${process.argv[2]} collection!`);
+    console.log(`Successfully imported data to ${argv._[1]} collection!`);
   } catch (err) {
     console.log(err);
   }
@@ -75,18 +74,39 @@ const importData = async (query) => {
 const deleteData = async (query) => {
   try {
     await query;
-    console.log(`Data deleted from ${process.argv[2]} collection.`);
+    console.log(`Data deleted from ${argv._[1]} collection.`);
   } catch (err) {
     console.log(err);
   }
   process.exit();
 };
 
+// Find document _id matching argument given
+const fleetName = argv._[3];
+
+// Load file from path specified
+const loadFile = () => {
+  const data = JSON.parse(fs.readFileSync(argv._[2], 'utf-8'));
+  return data;
+};
+
 // Import or Delete based on cmd line args
-if (process.argv[3] === '--import') {
-  const query = model.create(jsonFile);
-  importData(query);
-} else if (process.argv[3] === '--delete') {
+if (argv._[0] === 'import') {
+  const jsonFile = loadFile();
+  // Find parent _id in db
+  Customer.findOne({ name: fleetName }).then((customer) => {
+    const ID = JSON.stringify(customer._id);
+    // Update each document with new parent reference
+    const updatedFile = [];
+    jsonFile.forEach((el) => {
+      el.customerID = ID;
+      updatedFile.push(el);
+    });
+    // Import updated documents to db
+    const query = model.create(updatedFile);
+    importData(query);
+  });
+} else if (argv._[0] === 'delete') {
   const query = model.deleteMany();
   deleteData(query);
 } else {
