7df1e20afbddde28ce738aa08360c6f7f70223dd
Kris Sullivan <55643073+ksully1993@users.noreply.github.com>
2021-02-01 16:56:09 -0500

    Update mongoose-import-data.js

--- mongoose-import-data.js
+++ mongoose-import-data.js
@@ -1,7 +1,17 @@
 /* script to import json file data to db using mongoose models
+
 command line argument format:
-node import-data.js command model "name" path/to/file
-Example: node import-data.js import Vehicle "Glasco Heating & Air Conditionging" data/vehicle-data.json
+node import-data.js command model path/to/seed-file "fleetName"
+
+COMMANDS: 
+"import" - Import documents to db without reference IDs
+"update" - add fleetID reference to sub documents
+"updateFleet" - add sub document IDs to fleet documents after importing sub documents
+"delete" - delete all documents from collection
+
+MODELS:
+"Unit", "Fleet", "Service", "Operator"
+
 DB and password should be defined in config.env file
 Example: DATABASE=mongodb://localhost:27017/yourdbname
          DATABASE_PASSWORD=yourdbpassword */
@@ -11,13 +21,11 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const argv = require('minimist')(process.argv.slice(2));
 
-// Models already defined in separate files
-const Vehicle = require('../models/vehicleModel');
-const Customer = require('../models/customerModel');
+const Unit = require('../models/unitModel');
+const Fleet = require('../models/fleetModel');
 const Service = require('../models/serviceModel');
 const Operator = require('../models/operatorModel');
 
-// path to config.env file(from working directory, not relative path)
 dotenv.config({ path: './config.env' });
 
 // Connect to DB
@@ -42,11 +50,11 @@ mongoose
 // Select correct model for query
 let model = null;
 switch (argv._[1]) {
-  case 'Vehicle':
-    model = Vehicle;
+  case 'Unit':
+    model = Unit;
     break;
-  case 'Customer':
-    model = Customer;
+  case 'Fleet':
+    model = Fleet;
     break;
   case 'Service':
     model = Service;
@@ -81,35 +89,80 @@ const deleteData = async (query) => {
   process.exit();
 };
 
-// Find document _id matching argument given
+// Name of fleet
 const fleetName = argv._[3];
 
-// Load file from path specified
+// Load seed file from path specified
 const loadFile = () => {
   const data = JSON.parse(fs.readFileSync(argv._[2], 'utf-8'));
   return data;
 };
 
 if (argv._[0] === 'import') {
-  // Import documents to db
+  // Import documents to db without reference IDs
   const jsonFile = loadFile();
   const query = model.create(jsonFile);
   importData(query);
 } else if (argv._[0] === 'update') {
+  // add fleetID reference to sub documents
   const jsonFile = loadFile();
   // Find parent _id in db
-  Customer.findOne({ name: fleetName }).then((customer) => {
-    const ID = customer._id;
-    // Update each document with new parent reference
+  Fleet.findOne({ name: fleetName }).then((fleet) => {
+    const fleet_ID = fleet._id;
+    // Update each document with new fleet reference
     const updatedFile = [];
     jsonFile.forEach((el) => {
-      el.customerID = ID;
+      el.fleetID = fleet_ID;
       updatedFile.push(el);
     });
     // Import updated documents to db
     const query = model.create(updatedFile);
     importData(query);
   });
+} else if (argv._[0] === 'updateFleet') {
+  // add sub document IDs to fleet data after importing sub documents
+  let unitsArray = [];
+  let fleetId;
+  // find fleet by id
+  Fleet.findOne({ name: fleetName })
+    .then(async (fleet) => {
+      fleetId = fleet._id;
+      console.log(`FleetID: ${fleetId}`);
+      // find vehicles with fleetID matching fleet._id
+      const vehicles = await Vehicle.find({ fleetID: fleetId }, function (
+        err,
+        result
+      ) {
+        if (err) {
+          console.log(err);
+        } else {
+          return result;
+        }
+      });
+      // add the unit._id to fleet units array
+      vehicles.forEach((el) => {
+        unitsArray.push(el._id);
+        console.log(`Unit found: ${el._id}`);
+      });
+    })
+    .then(async () => {
+      const updatedFleet = await Fleet.findByIdAndUpdate(fleetId, {
+        units: unitsArray,
+      });
+      console.log('Updating fleet...');
+      return updatedFleet;
+    })
+    .then((updatedFleet) => {
+      console.log(
+        `${updatedFleet}\nFleet updated successfully with ${updatedFleet.units.length} units!`
+      );
+      process.exit();
+    })
+    .catch((err) => {
+      console.log(err);
+      process.exit();
+    });
+
 } else if (argv._[0] === 'delete') {
   // delete all documents from collection
   const query = model.deleteMany();
